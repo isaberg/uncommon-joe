d5331beb0b5432c5c5b16d2e59bd55ab5ff26fb8
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-06-14 12:16:20 -0400

    file structure, updated index.js file

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const express = require("express")
+//require JSON packages, define as variables so they can invoked
+const express = require('express')
 const app = express()
+const hbs = require('hbs')
+const bcrypt = require('bcrypt')
+const favicon = require('serve-favicon')
+const mongodb = require('mongodb')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000")
+//routes directory imports
+var applicationRouter = require('./routes/application.js')
+var indexRouter = require('./routes/index.js')
+var userRouter = require('./routes/user.js')
+var matchRouter = require('./routes/match.js')
+var messageRouter = require('./routes/message.js')
+var orgRouter = require('./routes/org.js')
+
+//view engine setup
+app.set('view engine', 'hbs')
+
+
+//routes
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static('public'))
+
+app.listen(4000, () => {
+  console.log("app listening success on port 4000")
 })
-
-
 console.log("Hello world from index.js")
