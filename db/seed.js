ffb9c43c644ace1bfcd7eb5943d85d1d3d626c2e
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-06-21 14:16:48 -0400

    fixed method and action on views

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 const User = require('../models/User.js')
+const Org = require('../models/Org.js')
 const bcrypt = require('bcrypt-nodejs')
 
 const createPassword = password =>
@@ -11,7 +12,7 @@ User.find({}).remove(() => {
       password: createPassword('testpassword123'),
       firstName: 'Isaiah',
       lastName: 'Berg',
-      zip: '58377'
+      zip: '12345'
     }
   }).then((user) => {
     user.save(err => console.log(err))
@@ -23,8 +24,53 @@ User.find({}).remove(() => {
       password: createPassword('testpassword1234'),
       firstName: 'Nathan',
       lastName: 'Berg',
-      zip: '58377'
+      zip: '12345'
     }}).then((user) => {
     user.save(err => console.log(err))
+  })
+
+  let marta = User.create({
+    local: {
+      email: 'martaberg@email.com',
+      password: createPassword('testpassword1234'),
+      firstName: 'Marta',
+      lastName: 'Berg',
+      zip: '12345'
+    }}).then((user) => {
+    user.save(err => console.log(err))
+  })
+
+  let david = User.create({
+    local: {
+      email: 'davidberg@email.com',
+      password: createPassword('testpassword1234'),
+      firstName: 'David',
+      lastName: 'Berg',
+      zip: '12345'
+    }}).then((user) => {
+    user.save(err => console.log(err))
+  })
+})
+
+Org.find({}).remove(() => {
+  let generalAssembly = Org.create({
+    local: {
+      email: 'generalassembly@email.com',
+      password: createPassword('testpassword123'),
+      orgName: 'General Assembly',
+      orgZip: '20005'
+    }
+  }).then((org) => {
+    org.save(err => console.log(err))
+  })
+
+  let dcTech = Org.create({
+    local: {
+      email: 'dctech@email.com',
+      password: createPassword('testpassword1234'),
+      orgName: 'DC Tech Meetup',
+      orgZip: '20005'
+    }}).then((org) => {
+    org.save(err => console.log(err))
   })
 })
--- models/Org.js
+++ models/Org.js
@@ -1,3 +1,20 @@
 const mongoose = require("../db/connection.js")
 const bcrypt = require("bcrypt-nodejs")
 const Schema = mongoose.Schema
+
+const Org = new Schema({
+  local: {
+    email: {type: String, required: true, max: 100},
+    password: {type: String, required: true, max: 100},
+    orgName: {type: String, required: true, max: 100},
+    orgZip: {type: String, required: true, max: 100},
+  },
+  users: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+})
+
+module.exports = mongoose.model('Org', Org)
