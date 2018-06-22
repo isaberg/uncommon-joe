1f276f90ac9115f5faedcf8db3b4ca3a1e347af2
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-06-22 12:49:52 -0400

    updating models and controllers for match.index

--- controllers/match.js
+++ controllers/match.js
@@ -8,7 +8,7 @@ const passport = require('passport')
 // IF I POST @/MATCH... I WANT TO GENERATE A NEW MATCH AND REDIRECT TO THE SAME /MATCH PAGE, IN ONE VIEW
 // DISPLAY OF MATCH CARDS WITH MESSAGE FUNCTIONS IS CONDITIONED ON USER LOGIN
 exports.index = function (req, res) {
-  User.findOne({email: currentUser.local.email})
+  User.findOne({ _id: req.params.id })
     .populate({
       path: 'matches',
       options: { limit: 5, sort: { createdAt: -1 } }
@@ -18,8 +18,48 @@ exports.index = function (req, res) {
     })
 }
 
+// POST @/match/index to drive match creation, followed by redirect to /match/index
 exports.matchPOST = function (req, res) {
 
+  /* // example tweet creation
+    Tweet.create({
+      content: req.body.tweet.content,
+      author: req.user._id
+    }).then(tweet => {
+      req.user.tweets.push(tweet);
+      req.user.save(err => {
+        res.redirect(`/tweet/${tweet._id}`);
+      });
+    });
+  */ // example tweet creation function end
+  res.render('/')
+}
+
+// PUT @/match/index/id to add messages, followed by redirect to /match/index
+exports.matchUpdate = function (req, res) {
+  /* EXAMPLE TWEET UPDATE TO ADD COMMENTS
+  {
+    let { content } = req.body;
+    Tweet.findOne({ _id: req.params.id }).then(tweet => {
+      tweet.comments.push({
+        content,
+        author: req.user._id
+      });
+      tweet.save(err => {
+        res.redirect(`/tweet/${tweet._id}`);
+      });
+    });
+  }
+  */ // END OF EXAMPLE TWEET UPDATE ADD COMMENTS
+  res.render('/')
+}
+
+exports.requireAuth = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/')
+  }
 }
 
 // GET @/match/new for form to request match
--- routes/match.js
+++ routes/match.js
@@ -2,9 +2,18 @@ const express = require('express')
 const router = express.Router()
 const matchController = require('../controllers/match.js')
 // const myController = require('../controllers/myController.js');
+router.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  next()
+})
 
-router.get('/', matchController.index)
-router.post('/', matchController.matchPOST)
+//implemented
+router.get('/', matchController.requireAuth, matchController.index)
+router.post('/', matchController.requireAuth, matchController.matchPOST)
+router.put('/:id', matchController.requireAuth, matchController.update)
+
+
+//no functionality implemented yet
 router.get('/create', matchController.new)
 router.post('/create', matchController.newPost)
 router.get('/delete', matchController.delete)
